bdddbd6bb9bf4aa35fff8cb5b11eb29877413ae7
4wavea <4wavea@gmail.com>
2023-03-15 12:55:22 +0400

    Removed cell index comparator

--- CellIndex.java
+++ CellIndex.java
@@ -1,11 +1,6 @@
-import java.util.Comparator;
 import java.util.List;
 
 public class CellIndex {
-    public static final Comparator<List<Integer>> COMPARATOR = Comparator
-            .comparingInt(CellIndex::getRowIndex)
-            .thenComparingInt(CellIndex::getColumnIndex);
-
     public static int getRowIndex(List<Integer> cellIndex) {
         return cellIndex.get(0);
     }
