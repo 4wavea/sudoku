ff8ecf7e77176f5e16b3d5f0e7602c2e15c6af0f
4wavea <4wavea@gmail.com>
2023-03-15 13:06:59 +0400

    Changed sample access modifier and error handling

--- Util.java
+++ Util.java
@@ -2,18 +2,20 @@ import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Util {
-    public static <E> List<E> sample(Collection<E> c, int n, Random r) {
-        ArrayList<E> list = new ArrayList<>(c);
-        int length = list.size();
-        if (length < n) return null;
-        for (int i = length - 1; i >= length - n; --i) {
+    private static <E> List<E> sample(Collection<E> population, int k, Random r) {
+        int length = population.size();
+        if (k < 0 || k > length) {
+            throw new IllegalArgumentException("Sample larger than population or is negative");
+        }
+        ArrayList<E> list = new ArrayList<>(population);
+        for (int i = length - 1; i >= length - k; --i) {
             Collections.swap(list, i, r.nextInt(i + 1));
         }
-        return list.subList(length - n, length);
+        return list.subList(length - k, length);
     }
 
-    public static <E> List<E> sample(Collection<E> list, int n) {
-        return sample(list, n, ThreadLocalRandom.current());
+    public static <E> List<E> sample(Collection<E> population, int k) {
+        return sample(population, k, ThreadLocalRandom.current());
     }
 
     public static <E> List<E> shuffle(Collection<E> list) {
